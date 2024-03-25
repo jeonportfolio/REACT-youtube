15ff8d29c0a2597b64cb525170f27b87a498dd44
jeons <etoile929@naver.com>
2024-03-25 16:48:30 +0900

    REACT 영상 데이터 가져오기 완료

--- src/components/contents/Website.jsx
+++ src/components/contents/Website.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import { websiteText } from '../../data/website'
+import { Link } from 'react-router-dom'
+
+
 const Website = () => {
-  return (
-    <div>Website</div>
-  )
+      return (
+            <section id='website' >
+                <h2>😛 웹사이트</h2>
+                    <div className="video__inner">
+                        {websiteText.map((video, key) => (
+                              <div className="video" key={key}>
+                                    <div className="video__thumb play__icon">
+                                          <Link to={`/video/${video.videoId}`}>
+                                              <img src={video.img} alt={video.title} />
+                                          </Link>
+                                    </div>
+                              </div>
+                        ))}
+                    </div>
+             </section>
+      )
 }
 
 export default Website
