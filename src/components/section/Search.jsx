ab03b6053d9034a320fe34b883584a6fd542fe6a
jeons <etoile929@naver.com>
2024-03-28 20:55:37 +0900

    REACT 유튜브 검색 API 연동

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const navigate = useNavigate();
+
+  const handlesearch = () => {
+    console.log(searchKeyword);           //3. 여기로 정보 전달한다.
+    if(searchKeyword){
+        navigate(`/search/${searchKeyword}`);
+        setSearchKeyword('');
+    }
+  }
+
   return (
     <div className='search__inner'>
         <label htmlFor='searchInput'>
@@ -12,7 +24,13 @@ const Search = () => {
             id='searchInput'
             autoComplete='off'
             className='search__input'  
-            placeholder='검색어를 입력해주세요.'  
+            placeholder='검색어를 입력해주세요.'
+            onChange={e => setSearchKeyword(e.target.value)} //1.데이터 입력시 여기서 데이터가 바뀌고
+            onKeyDown={e => {
+              if(e.key === 'Enter'){            //2.여기서 엔터 쳤을떄 
+                  handlesearch();
+              }
+            }}  
         />
     </div>
   )
