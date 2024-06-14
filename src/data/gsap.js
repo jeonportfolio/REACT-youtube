483a30f9af0ae3012cc063511bb48336f3d6e774
jeons <etoile929@naver.com>
2024-06-14 18:08:52 +0900

    sqld 수정

--- src/data/gsap.js
+++ src/data/gsap.js
@@ -1,11 +1,7 @@
-import gsap01 from "../assets/img/gsap/gsap01.jpg";
-import gsap02 from "../assets/img/gsap/gsap02.jpg";
-import gsap03 from "../assets/img/gsap/gsap03.jpg";
-import gsap04 from "../assets/img/gsap/gsap04.jpg";
-import gsap05 from "../assets/img/gsap/gsap05.jpg";
-import gsap06 from "../assets/img/gsap/gsap06.jpg";
-import gsap07 from "../assets/img/gsap/gsap07.jpg";
-import gsap08 from "../assets/img/gsap/gsap08.jpg";
+
+
+import gsap07 from "../assets/img/gsap/gsap09.jpg";
+import gsap08 from "../assets/img/gsap/gsap09.jpg";
 import gsap09 from "../assets/img/gsap/gsap09.jpg";
 import gsap10 from "../assets/img/gsap/gsap10.jpg";
 import gsap11 from "../assets/img/gsap/gsap11.jpg";
@@ -16,61 +12,61 @@ import gsap15 from "../assets/img/gsap/gsap15.jpg";
 
 export const gsapText = [
     {
-        title: "01. GSAP Parallax Effect : 기본 애니메이션(feat. scrollTrigger) |  웹스토리보이",
-        img: gsap01,
-        author: "webstoryboy",
-        videoId: "_rAAY_Zp4FI",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SQLD 1과목 완벽 정리 (2024년 신유형 반영)",
+        img:"https://i.ytimg.com/vi/QB_GYdHUHmA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8dZkYdGngwAWF3HTtsPnca5_UIQ",
+        author: "홍씨의 데이터랩",
+        videoId: "QB_GYdHUHmA&t=98s",
+        date: "2024. 2. 14",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
-        title: "02. GSAP Parallax Effect : PIN 애니메이션(feat. scrollTrigger) | 웹스토리보이 |  포트폴리오",
-        img: gsap02,
-        author: "webstoryboy",
-        videoId: "T9kkvtHaq8E",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SQLD 2과목 PART1. SQL 기본 완벽 정리 (2024년 신유형 반영)",
+        img: "https://i.ytimg.com/vi/_f_WZYuNr7A/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD0dHmqIGQx5IdeKC9dPmtDhUAafA",
+        author: "홍씨의 데이터랩",
+        videoId: "_f_WZYuNr7A",
+        date: "2024. 2. 18",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
-        title: "03. GSAP Parallax Effect : PIN 배경 고정하기(feat. scrollTrigger) | 웹스토리보이 |  스크롤 효과",
-        img: gsap03,
-        author: "webstoryboy",
-        videoId: "bP7iGE3YKOU",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SQLD 2과목 PART2. SQL 활용 완벽 정리(2024 신유형 반영)",
+        img: "https://i.ytimg.com/vi/ijpxmi4DPj4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgsuLdgFh8Xm1SyoyOHgucV4vbkg",
+        author: "홍씨의 데이터랩",
+        videoId: "EXx6fjxycSY",
+        date: "2024. 2. 20",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
-        title: "04. GSAP Parallax Effect : 이질감 효과(feat. scrollTrigger) | 웹스토리보이 |  스크롤 효과",
-        img: gsap04,
-        author: "webstoryboy",
-        videoId: "neBcJlgfu_o",
+        title: "SQLD 2과목 PART3. 관리 구문 완벽정리",
+        img: "https://i.ytimg.com/vi/ijpxmi4DPj4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgsuLdgFh8Xm1SyoyOHgucV4vbkg",
+        author: "홍씨의 데이터랩",
+        videoId: "ijpxmi4DPj4",
         date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
-        title: "05. GSAP Parallax Effect : 나타나기 효과(feat. scrollTrigger) | 웹스토리보이 | 포폴 이펙트",
-        img: gsap05,
-        author: "webstoryboy",
-        videoId: "9lnfRXTXzL0",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SQLD 완벽정리 부록편",
+        img: "https://i.ytimg.com/vi/ovGGaIGL2Ys/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAHpqIF2l6XtG-AnD7KEAQHGL_Q3g",
+        author: "홍씨의 데이터랩",
+        videoId: "ovGGaIGL2Ys&t=904s",
+        date: " 2024. 3. 2",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
-        title: "06. GSAP Parallax Effect : 텍스트 효과(feat. scrollTrigger) | 웹스토리보이 | 텍스트 이펙트",
-        img: gsap06,
-        author: "webstoryboy",
-        videoId: "gzJnKZGsB-Y",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SQLD 기출 문제 풀이(신유형 예상 문제 포함)",
+        img: "https://i.ytimg.com/vi/5fTVqNUE5sg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD-nCTvbnDV6T14ug96zRpwWkJiVA",
+        author: "홍씨의 데이터랩",
+        videoId: "5fTVqNUE5sg",
+        date: " 2024. 3. 2",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
         title: "07. GSAP Parallax Effect : 배경색 효과(feat. scrollTrigger) | 웹스토리보이 | 배경 바꾸기",
         img: gsap07,
         author: "webstoryboy",
         videoId: "kE0QRWPBVKs",
         date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
         title: "08. GSAP Parallax Effect : 진행바 효과(feat. scrollTrigger) | 웹스토리보이 | progress",
         img: gsap08,
         author: "webstoryboy",
         videoId: "pLgDSor_27g",
         date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        channelId: "UC-BQkV5Hpxq4tP1H2ZIurhw",
     },{
         title: "09. GSAP Parallax Effect : 메뉴 이동 효과 (feat. scrollTrigger) | 웹스토리보이 | 메뉴 클릭하기",
         img: gsap09,
