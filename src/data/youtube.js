0c99ec94d1d7b9f2549ec04214c941bc76483859
jeons <etoile929@naver.com>
2024-07-05 19:16:18 +0900

    음악파트 수정 완료

--- src/data/youtube.js
+++ src/data/youtube.js
@@ -1,8 +1,12 @@
 import youtube01 from "../assets/img/youtube/gift.png";
 import youtube02 from "../assets/img/youtube/dong.png";
-import youtube03 from "../assets/img/youtube/youtube03.jpg";
-import youtube04 from "../assets/img/youtube/youtube04.jpg";
-import youtube05 from "../assets/img/youtube/youtube05.jpg";
+import youtube03 from "../assets/img/youtube/wind.png";
+import youtube04 from "../assets/img/youtube/love.png";
+import youtube05 from "../assets/img/youtube/feel.png";
+import youtube06 from "../assets/img/youtube/wake.png";
+import youtube07 from "../assets/img/youtube/episode.png";
+import youtube08 from "../assets/img/youtube/tear.png";
+
 
 export const youtubeText = [
     {
@@ -20,46 +24,46 @@ export const youtubeText = [
         date: "2014.10.01",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
-        title: "03 나만의 유튜브 사이트 만들기",
+        title: "권영찬 - 바람,노래",
         img: youtube03,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
+        author: "권영찬",
+        videoId: "7Eywi03rLa4",
+        date: "2019.07.12",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
-        title: "04 나만의 유튜브 사이트 만들기",
+        title: "아이유 - LOVE WINS ALL",
         img: youtube04,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        author: "아이유",
+        videoId: "ax1csKKQnns",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
-        title: "05 나만의 유튜브 사이트 만들기",
+        title: "Gryffin, Illenium - FEEL GOOD",
         img: youtube05,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
+        author: "Gryffin, Illenium",
+        videoId: "sZLLB6JNKpU",
+        date: "2017.03.04",
+        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+    },{
+        title: "tommee proffit - WAKE ME UP",
+        img: youtube06,
+        author: "tommee proffit",
+        videoId: "tOjIa5PLqDo",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
-        title: "06 나만의 유튜브 사이트 만들기",
-        img: youtube01,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
+        title: "이무진 - 에피소드",
+        img: youtube07,
+        author: "이무진",
+        videoId: "MjXeOAouF3w",
+        date: "2023.12.13",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
-        title: "07 나만의 유튜브 사이트 만들기",
-        img: youtube02,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "08 나만의 유튜브 사이트 만들기",
-        img: youtube03,
-        author: "webstoryboy",
-        videoId: "OKZXYgAgtyw",
-        date: "2023.09.09",
+        title: "크러쉬 - 미안해 미워해 사랑해",
+        img: youtube08,
+        author: "크러쉬",
+        videoId: "Z4qVfCi_lUI",
+        date: "2024. 3. 24",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     }
 ]
