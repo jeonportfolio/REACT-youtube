d9adf80082a7397d5bd56d1dd755b514a6fa6088
jeons <etoile929@naver.com>
2024-07-07 16:46:39 +0900

    SQLD 사이트 완료

--- src/data/today.js
+++ src/data/today.js
@@ -1,6 +1,6 @@
 
 
-import todayView03 from "../assets/img/today/todayView03.jpg";
+
 import todayView04 from "../assets/img/today/todayView04.jpg";
 import todayView05 from "../assets/img/today/todayView05.jpg";
 import todayView06 from "../assets/img/today/todayView06.jpg";
@@ -25,13 +25,13 @@ export const todayText = [
         date: "2023. 3. 3",
         page: "/port"
     },{
-        img: todayView03,
-        title: "React.js를 이용하여 포트폴리오 사이트 만들기",
-        desc : "안녕하세요, 여러분! 오늘은 특별한 프로젝트를 소개해드리려고 합니다. 제가 '나만의 코딩 유튜브 사이트 만들기'라는 주제로 프로젝트를 진행하게 되었는데요, 이 프로젝트는 저의 열정인 코딩과 온라인 교육을 결합한 결과물입니다. 이 프로젝트의 주요 목표는 누구나 쉽게 접근하고 배울 수 있는 공간을 만드는 것입니다. 제가 여러 해 동안 프로그래밍을 공부하면서 느낀 점은 양질의 학습 자료와 개인의 진도에 맞는 커리큘럼이 얼마나 중요한지입니다. 그래서 이 사이트에서는 초보자부터 고급 수준까지 다양한 난이도와 주제의 온라인 강의를 제공할 계획입니다.",
-        author : "webstoryboy",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-        date: "2023.07.07",
-        page: "/port"
+        img: "https://i.ytimg.com/vi/G3DpvBUK014/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAckX-YioQGAdRQibDLTrihoSyErw",
+        title: "컴퓨터활용능력 2급 해설",
+        desc : "컴퓨터활용능력 2급 기출 해설입니다.",
+        author : "박쌤컴퓨터",
+        channelId: "UCvgU7moT0m89iDvjRolLcyg",
+        date: "2023.12.23",
+        page: "/website"
     },{
         img: todayView04,
         title: "Vue.js를 이용하여 포트폴리오 사이트 만들기",
